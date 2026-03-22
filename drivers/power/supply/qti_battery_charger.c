682dc2f5381eca2045ae3869147797b656d1c754
Igor Belwon <igor.belwon@mentallysanemainliners.org>
2026-03-22 15:46:37 +0100

    power: supply: qti_battery_charger: Import vendor changes
    From 69.2.A.2.59
    
    Change-Id: I3bf5195220c8369153c715be740ffee7729e67c1

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -508,6 +508,7 @@ static int get_property_id(struct psy_state *pst,
 	return -ENOENT;
 }
 
+#if 0
 static void battery_chg_notify_disable(struct battery_chg_dev *bcdev)
 {
 	struct battery_charger_set_notify_msg req_msg = { { 0 } };
@@ -526,6 +527,7 @@ static void battery_chg_notify_disable(struct battery_chg_dev *bcdev)
 			bcdev->notify_en = false;
 	}
 }
+#endif
 
 static void battery_chg_notify_enable(struct battery_chg_dev *bcdev)
 {
@@ -1158,6 +1160,22 @@ static int usb_psy_get_prop(struct power_supply *psy,
 	if (prop == POWER_SUPPLY_PROP_TEMP)
 		pval->intval = DIV_ROUND_CLOSEST((int)pval->intval, 10);
 
+	if (prop == POWER_SUPPLY_PROP_ONLINE) {
+		rc = read_property_id(bcdev, pst, USB_ADAP_TYPE);
+		if (rc < 0) {
+			pr_err("Failed to read prop USB_ADAP_TYPE, rc=%d\n",
+									rc);
+			return 0;
+		}
+
+		if (pval->intval && pst->prop[USB_ADAP_TYPE] ==
+						POWER_SUPPLY_USB_TYPE_UNKNOWN) {
+			pr_debug("Since usb_type property is unknown, set online property to 0\n");
+			pval->intval = 0;
+		}
+	}
+
+
 	return 0;
 }
 
@@ -1446,8 +1464,12 @@ static int battery_psy_get_prop(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_CAPACITY:
 		pval->intval = DIV_ROUND_CLOSEST(pst->prop[prop_id], 100);
-		if (IS_ENABLED(CONFIG_QTI_PMIC_GLINK_CLIENT_DEBUG) &&
-		   (bcdev->fake_soc >= 0 && bcdev->fake_soc <= 100))
+#if 0
+ 		if (IS_ENABLED(CONFIG_QTI_PMIC_GLINK_CLIENT_DEBUG) &&
+ 		   (bcdev->fake_soc >= 0 && bcdev->fake_soc <= 100))
+#else
+		if (bcdev->fake_soc >= 0 && bcdev->fake_soc <= 100)
+#endif
 			pval->intval = bcdev->fake_soc;
 		break;
 	case POWER_SUPPLY_PROP_TEMP:
@@ -2119,7 +2141,11 @@ static ssize_t fake_soc_store(struct class *c, struct class_attribute *attr,
 	bcdev->fake_soc = val;
 	pr_debug("Set fake soc to %d\n", val);
 
+#if 0
 	if (IS_ENABLED(CONFIG_QTI_PMIC_GLINK_CLIENT_DEBUG) && pst->psy)
+#else
+	if (pst && pst->psy)
+#endif
 		power_supply_changed(pst->psy);
 
 	return count;
@@ -2350,9 +2376,14 @@ static int battery_chg_parse_dt(struct battery_chg_dev *bcdev)
 
 	rc = read_property_id(bcdev, pst, BATT_CHG_CTRL_LIM_MAX);
 	if (rc < 0) {
-		pr_err("Failed to read prop BATT_CHG_CTRL_LIM_MAX, rc=%d\n",
-			rc);
-		return rc;
+		/* This process may fail depending on the timing.
+		 *  If this fails, the property will not be registered.
+		 *  Work around this issue by changing the return
+		 *  value from rc to -EPROBE_DEFER.
+		 */
+ 		pr_err("Failed to read prop BATT_CHG_CTRL_LIM_MAX, rc=%d\n",
+ 			rc);
+		return -EPROBE_DEFER;
 	}
 
 	rc = of_property_count_elems_of_size(node, "qcom,thermal-mitigation",
@@ -2461,6 +2492,7 @@ static int battery_chg_ship_mode(struct notifier_block *nb, unsigned long code,
 	return NOTIFY_DONE;
 }
 
+#if 0
 static void panel_event_notifier_callback(enum panel_event_notifier_tag tag,
 			struct panel_event_notification *notification, void *data)
 {
@@ -2533,6 +2565,7 @@ static int battery_chg_register_panel_notifier(struct battery_chg_dev *bcdev)
 	bcdev->notifier_cookie = cookie;
 	return 0;
 }
+#endif
 
 static int
 battery_chg_get_max_charge_cntl_limit(struct thermal_cooling_device *tcd,
@@ -2570,6 +2603,31 @@ static const struct thermal_cooling_device_ops battery_tcd_ops = {
 	.get_cur_state = battery_chg_get_cur_charge_cntl_limit,
 	.set_cur_state = battery_chg_set_cur_charge_cntl_limit,
 };
+
+static ssize_t
+state_of_health_show(struct device *dev,
+                     struct device_attribute *attr, char *buf)
+{
+	int rc, soh = 0;
+	struct power_supply *psy;
+	union power_supply_propval charge_full, charge_full_design;
+
+	psy = container_of(dev, struct power_supply, dev);
+	if (!psy)
+		return -ENOENT;
+	rc = battery_psy_get_prop(psy, POWER_SUPPLY_PROP_CHARGE_FULL,
+	                          &charge_full);
+	if (rc < 0)
+		goto out;
+	rc = battery_psy_get_prop(psy, POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN,
+	                          &charge_full_design);
+	if (rc == 0 && charge_full_design.intval > 0)
+		soh = DIV_ROUND_CLOSEST(charge_full.intval * 100,
+		                        charge_full_design.intval);
+out:
+	return scnprintf(buf, PAGE_SIZE, "%d\n" , soh);
+}
+static DEVICE_ATTR_RO(state_of_health);
 
 static int battery_chg_probe(struct platform_device *pdev)
 {
@@ -2626,9 +2684,11 @@ static int battery_chg_probe(struct platform_device *pdev)
 	INIT_WORK(&bcdev->battery_check_work, battery_chg_check_status_work);
 	bcdev->dev = dev;
 
+#if 0
 	rc = battery_chg_register_panel_notifier(bcdev);
 	if (rc < 0)
 		return rc;
+#endif
 
 	client_data.id = MSG_OWNER_BC;
 	client_data.name = "battery_charger";
@@ -2671,6 +2731,9 @@ static int battery_chg_probe(struct platform_device *pdev)
 	rc = battery_chg_init_psy(bcdev);
 	if (rc < 0)
 		goto error;
+
+	device_create_file(&bcdev->psy_list[PSY_TYPE_BATTERY].psy->dev,
+	                   &dev_attr_state_of_health);
 
 	bcdev->battery_class.name = "qcom-battery";
 
